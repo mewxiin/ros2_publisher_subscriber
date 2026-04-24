ac64e05f0243dc8adfb493642af08b4fe359058e
mewxiin <140331543+mewxiin@users.noreply.github.com>
2026-04-24 17:35:07 +0500

    Changes in setup.py and .cfg

--- setup.py
+++ setup.py
@@ -4,25 +4,20 @@ package_name = 'ros2_publisher_subscriber'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    package_data={'': ['py.typed']},
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mewxiin',
-    maintainer_email='mewxiin@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='mewxiin@gmail.com',
+    description='A ROS 2 messaging system implementing a Publisher-Subscriber architecture.',
+    license='',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'publisher = ros2_publisher_subscriber.publisher:main',
